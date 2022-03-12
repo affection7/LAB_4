bdd95c914baad4468e0e5c0e5d4442b588f986a7
Animakkon <lada.dunskaya@mail.ru>
2022-03-12 14:45:31 +0300

    ref: some todos

--- lib/main.dart
+++ lib/main.dart
@@ -3,42 +3,49 @@ import 'dart:math';
 
 class MyBody extends StatefulWidget {
   @override
-  createState() => new MyBodyState();
+  createState() => MyBodyState();
 }
 
 class MyBodyState extends State<MyBody> {
-
   @override
   Widget build(BuildContext context) {
-
     return ListView.builder(itemBuilder: (context, i) {
       print('num $i : нечетное = ${i.isOdd}');
 
-      if (i.isOdd) return new Divider(
-        color: Colors.amber,
-      );
+//  todo: рекомендую к прочтению книги "Чистый код".
+//        представим, что второму разработчику придется модифицировать оператор
+// сравнения. Таким образом придется переписывать его
+      if (i.isOdd)
+        return new Divider(
+          color: Colors.amber,
+        );
       final int index = i ~/ 2;
 
       index.toDouble();
 
+// todo: ненужное вычищается
+
       //if (index >= _array.length)
-        //_array.addAll(['$index', '${index + 1}', '${index + 2}']);
-      num x = pow(2,index);
+      //_array.addAll(['$index', '${index + 1}', '${index + 2}']);
+      num x = pow(2, index);
       return Text('2' '^' '$index' ' ' '=' '$x');
     });
   }
 }
 
-void main() =>
-    runApp(MaterialApp(
-        debugShowCheckedModeBanner: false,
-        home: Scaffold(
-          appBar: AppBar(
+// todo: ну, запрятали... возвращайте на место,
+//       чтобы читался код и логика сверху вниз
+void main() => runApp(MaterialApp(
+    debugShowCheckedModeBanner: false,
+    home: Scaffold(
+        appBar: AppBar(
             title: Container(
-              child:Text('Список элементов', style: TextStyle(color: Colors.black),),
-
-            )
+          // todo: соблюдаем отступы!
+          child: Text(
+            'Список элементов',
+            style: TextStyle(color: Colors.black),
           ),
-            body: new Center(
-              child: new MyBody(),
-            ))));
+        )),
+        body: Center(
+          child: MyBody(),
+        ))));
